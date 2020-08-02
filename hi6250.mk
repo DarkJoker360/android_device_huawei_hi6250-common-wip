10efd1103f3e7bbe412ed2e75a97b55e0b167f75
hak86 <hakamelassouad@gmail.com>
2020-08-02 21:53:30 +0200

    hi6250-common: add fake kernel to pass build

--- hi6250.mk
+++ hi6250.mk
@@ -52,6 +52,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fingerprint.kl:system/usr/keylayout/fingerprint.kl
 
+# Kernel
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/kernel/dummykernel:kernel
+
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
